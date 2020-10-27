090d651906605023a144942ebae745fe11cbdc39
Hugo Tameirão Seixas <50832491+hugotseixas@users.noreply.github.com>
2020-10-27 20:42:34 -0300

    Update 02_raster_to_table.R

--- 02_raster_to_table.R
+++ 02_raster_to_table.R
@@ -49,6 +49,12 @@ source("00_config.R")
 
 tic()
 
+####' ----- Convert mask raster to table ####
+mask <- raster('data/original_raster/mask.tif') %>%
+  tabularaster::as_tibble(xy = TRUE, cell = TRUE) %>%
+  filter(!(cellvalue == 0)) %>%
+  select(-cellvalue)
+
 ####' ----- Function to transform raster data to table ####
 raster_to_table <- function(raster_var) {
 
@@ -62,9 +68,9 @@ raster_to_table <- function(raster_var) {
       cellindex = integer(),
       x = double(),
       y = double(),
-      year = integer()
+      date = character()
     ),
-    glue('data/tables/var_{raster_var}.txt'),
+    glue('data/tables/{raster_var}.txt'),
     delim = ','
   )
 
@@ -79,28 +85,30 @@ raster_to_table <- function(raster_var) {
 
     # Load as a stack and get its bands number
     raster_stack <- stack(path)
-    raster_bands <- seq_along(names(raster_stack))
+    raster_bands <- names(raster_stack)
 
     # Loop trough bands
-    for (band_num in raster_bands) {
+    for (band_num in seq_along(raster_bands)) {
 
       cat('\r', 'Band: ', band_num, '  ')
 
       # Load raster
       raster_chunk <- raster(path, band = band_num)
 
-      # Transform to table
+      # Transform to table and join with mask table
       raster_table <- raster_chunk %>%
         tabularaster::as_tibble(xy = TRUE, cell = TRUE) %>%
-        drop_na() %>% # Remove cells with no values
-        filter(!(cellvalue == 0)) %>%
-        rename(!!sym(raster_var) := cellvalue) %>%
-        mutate(month_num = band_num)
+        right_join(mask, by = c('x', 'y', 'cellindex')) %>%
+        mutate(
+          date = str_remove(raster_bands[band_num], "X"),
+          cellvalue = if_else(is.nan(cellvalue), NA_real_, cellvalue)
+        ) %>%
+        rename(!!sym(raster_var) := cellvalue)
 
       # Append to table created before
       write_delim(
         raster_table,
-        glue('data/tables/var_{raster_var}.txt'),
+        glue('data/tables/{raster_var}.txt'),
         delim = ',',
         append = TRUE
       )
@@ -117,17 +125,18 @@ raster_to_table <- function(raster_var) {
 variables <- dir_ls('data/original_raster/') %>%
   str_extract("(?<=raster/)[^-|.]+") %>%
   as_tibble() %>%
+  filter(value != 'mask') %>%
   distinct(value) %>%
   pull(value)
 
 ####' ----- Apply function to all variables ####
 if (multi_thread == TRUE) {
 
-  plan(strategy = "multiprocess", workers = workers_num)
+  plan(strategy = "multisession", workers = workers_num)
 
-  future_walk(variables, raster_to_table)
+  future_walk(variables, raster_to_table, .options = furrr_options(seed = NULL))
 
-} else if(multi_thread == FALSE) {
+} else if (multi_thread == FALSE) {
 
   walk(variables, raster_to_table)
 
